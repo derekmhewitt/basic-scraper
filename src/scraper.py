6b5302e674703118d3be7a9f2686dda92a62f5fc
Derek Hewitt <derekmhewitt@gmail.com>
2016-10-17 16:40:59 -0700

    filtering out empty strings

--- src/scraper.py
+++ src/scraper.py
@@ -59,8 +59,27 @@ def parse_source(html, encoding="utf-8"):
 
 
 def extract_data_listings(html):
+    """extracts divs where the id matches PR(some junk)"""
     id_finder = re.compile(r'PR[\d]+~')
     return html.find_all('div', id=id_finder)
+
+
+def has_two_tds(element):
+    """finds table rows that have only two children"""
+    is_tr = element.name == 'tr'
+    td_children = element.find_all('td', recursive=False)
+    has_two = len(td_children) == 2
+    return is_tr and has_two
+
+
+def clean_data(td):
+    """cleans empty strings out of the data we've scraped"""
+    data = td.string
+    try:
+        return data.strip(" \n:-")
+    except AttributeError:
+        return u""
+
 
 if __name__ == '__main__':
     kwargs = {
@@ -74,5 +93,9 @@ if __name__ == '__main__':
         html, encoding = get_inspection_page(**kwargs)
     doc = parse_source(html, encoding)
     listings = extract_data_listings(doc)
-    print("How many restraunts:", len(listings))
-    print(listings[0].prettify())
+    for listing in listings[:5]:
+        metadata_rows = listing.find('tbody').find_all(
+            has_two_tds, recursive=False)
+    for row in metadata_rows:
+        for td in row.find_all('td', recursive=False):
+            print repr(clean_data(td))
